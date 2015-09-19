f4525e49c95fcf45ad879beaca9d2b48769dc613
Brukhan <aj-nasaj@mail.ru>
2015-09-19 21:34:37 +0300

    Search univ and facult

--- lib/vkontake_searcher.rb
+++ lib/vkontake_searcher.rb
@@ -11,6 +11,20 @@ class VkontakteSearcher
   end
 
   def display_univers_data
+    token = "716ddd52d747b62ab5ebd594d25b7f2d3020c85f1a84d59d4302fd4a238eb072ce5f732374a8ec2279206"
 
+    puts "Parsing university..."
+
+    univ = vk.database.getUniversities(city_id: 282)
+
+    univ.id.each do |idd|
+      puts idd
+      facultet = vk.database.getFaculties(university_id: #{idd})
+      facultet.each do |facIdd|
+        puts facIdd
+      end
+    end
+
+    puts "Done."
   end
 end
